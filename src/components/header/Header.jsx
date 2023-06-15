74faca54b8d4ca6edffb019a5d74ea86522b2125
David AVILA ALVAREZ <davissaix@gmail.com>
2023-06-15 01:27:33 -0500

    header centered, side bar done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import polyTranslate from '../../assets/PolyTranslate.png'
-import { ViewPort } from './styles'
+import { ViewPort, Container } from './styles'
 
 const Header = () => {
   return (
     <div>
+      <Container>
         <ViewPort src={polyTranslate} alt='logo-polyTranslate'/>
+      </Container>
     </div>
   )
 }
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -4,5 +4,13 @@ export const ViewPort = styled.img`
     
         width: 100vw;
         height: 12vh;
-    
+        @media screen and (min-width: 580px) {
+            width: 580px;
     `
+    export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 12vh;
+  `;    
